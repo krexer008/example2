f94c4962acc145f49e81f62186b962bff30c3040
krexer008 <y.fedor86@mail.ru>
2024-01-14 00:14:28 +0300

    Lesson complete

---
+++ src/components/Card.tsx
@@ -0,0 +1,40 @@
+import React, { FC, useState } from 'react';
+
+export enum CardVariant {
+    outlined = 'outlined',
+    primary = 'primary',
+}
+
+interface CardProps {
+    width?: string;
+    height?: string;
+    variant: CardVariant,
+    children?: React.ReactNode,
+    onClick: (num: number) => void,
+}
+
+const Card: FC<CardProps> =
+        ({
+             width,
+             height,
+             children,
+             variant,
+             onClick,
+         }) => {
+            const [state, seState] = useState(0);
+            return (
+                    <div
+                            style={{
+                                width,
+                                height,
+                                border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
+                                background: variant === CardVariant.primary ? 'lightgray' : ''
+                            }}
+                            onClick={() => onClick(state)}
+                    >
+                        {children}
+                    </div>
+            );
+        };
+
+export default Card;
---
+++ src/components/UserList.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import { IUser } from "../types/types";
+import UserItem from "./UserItem";
+
+interface UserListProps {
+    users: IUser[];
+}
+
+const UserList: FC<UserListProps> = ({ users }) => {
+
+    return (
+            <div>
+
+            </div>
+    );
+};
+
+export default UserList;
